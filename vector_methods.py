c21cbc6fcd23f0c8002048665ac3797b69c16c96
Rodney Moore <rodney@nerdsindenial.com>
2025-03-16 17:02:04 -0700

    new file:   vector_methods.py

---
+++ vector_methods.py
@@ -0,0 +1,21 @@
+import numpy as np
+import os
+
+class vm:
+  pass
+       
+
+
+if __name__ == '__main__':
+  uMatrix = np.random.randint(-20, 20, size=(3, 3))
+  vMatrix = np.random.randint(-20, 20, size=(3, 3))
+  wMatrix = np.random.randint(-20, 20, size=(3, 3))
+ 
+
+  uVector = np.random.randint(-20, 20, size=(3))
+  vVector = np.random.randint(-20, 20, size=(3))
+  wVector = np.random.randint(-20, 20, size=(3))
+ 
+
+
+  print(uMatrix)
